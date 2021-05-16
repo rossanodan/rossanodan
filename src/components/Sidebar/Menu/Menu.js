f3efff9003f21828f6927b0ab5b7d6b1c8ae48a7
Rossano D'Angelo <rossanodan@Rossanos-MacBook-Air.local>
2021-05-16 14:53:45 +0100

    pr-issue/9: added dev link as part of main menu

--- src/components/Sidebar/Menu/Menu.js
+++ src/components/Sidebar/Menu/Menu.js
@@ -6,7 +6,8 @@ import styles from './Menu.module.scss';
 type Props = {
   menu: {
     label: string,
-    path: string
+    path: string,
+    target: string
   }[]
 };
 
@@ -15,13 +16,23 @@ const Menu = ({ menu }: Props) => (
     <ul className={styles['menu__list']}>
       {menu.map((item) => (
         <li className={styles['menu__list-item']} key={item.path}>
-          <Link
-            to={item.path}
-            className={styles['menu__list-item-link']}
-            activeClassName={styles['menu__list-item-link--active']}
-          >
-            {item.label}
-          </Link>
+          {item.target === 'external' ? (
+            <a
+              href={item.path}
+              target="_blank"
+              className={styles['menu__list-item-link']}
+            >
+              {item.label}
+            </a>
+          ) : (
+            <Link
+              to={item.path}
+              className={styles['menu__list-item-link']}
+              activeClassName={styles['menu__list-item-link--active']}
+            >
+              {item.label}
+            </Link>
+          )}
         </li>
       ))}
     </ul>
