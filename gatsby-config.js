5f962ac27480aaa3746863930d694cbd9352edfa
Rossano D'Angelo <rossanodan@Rossanos-MacBook-Air.local>
2021-05-16 18:00:07 +0100

    master: updating dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -137,16 +137,6 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/index.js`
       }
     },
-    // TODO: To remove gatsby-plugin-google-gtag from dependecies
-    // {
-    //   resolve: 'gatsby-plugin-google-gtag',
-    //   options: {
-    //     trackingIds: [siteConfig.googleAnalyticsId],
-    //     pluginConfig: {
-    //       head: true
-    //     }
-    //   }
-    // },
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
