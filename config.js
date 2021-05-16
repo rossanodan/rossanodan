8ffd12b32afc87d21bb3380e0faedcd77c4e409d
Rossano D'Angelo <rossanodan@Rossanos-MacBook-Air.local>
2021-05-16 16:12:21 +0100

    pr-issue/5: added google analytics track id

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: '',
+  googleAnalyticsId: 'UA-193595709-1',
   useKatex: false,
   menu: [
     {
